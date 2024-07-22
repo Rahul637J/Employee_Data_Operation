e4bdcd92535464259a8f39e139b0811cebd1edf0
Rahul Jaganathan <rahul637j@gmail.com>
2024-07-22 23:18:54 +0530

    [Rahul] Added_Monthly_Wages

--- src/com/employee/data/Demo.java
+++ src/com/employee/data/Demo.java
@@ -9,33 +9,33 @@ public class Demo
 		int wagePerHour = 20;
         int fullDay = 8;
         int halfDay = 4;
+        int totalNoOfDays = 20;
         
         Random random=new Random();
         int attendance=random.nextInt(3);
         
-        if( attendance == 1 )
-        {
+        switch ( attendance ) {
+        case 0:
             System.out.print("Employee is Present");
             System.out.println("");
-            System.out.print("The daily wage of Employee is : " + ( wagePerHour * fullDay ));
+            System.out.print("The daily wage of Employee is : " + ( wagePerHour * fullDay * totalNoOfDays ));
             System.out.println("");
-        }
+            break;
 
-        else if ( attendance == 0 )
-
-        {
+        case 1:
             System.out.print("Employee is Present but working part time ");
             System.out.println("");
-            System.out.print("The daily wage of Employee is : " + ( wagePerHour * halfDay ));
+            System.out.print("The daily wage of Employee is : " + ( wagePerHour * halfDay * totalNoOfDays ));
             System.out.println("");
-        }
-        
-        else
-        {
+            break;
+
+        case 2:
             System.out.print("Employee is Absent");
             System.out.println("");
-            System.out.println("The daily wage of Employee is : 0 " );
+            System.out.println("The daily wage of Employee is : 0 ");
             System.out.println("");
-        }
+            break;
+
+       }
 	}
 }
